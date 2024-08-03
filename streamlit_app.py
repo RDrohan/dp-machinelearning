bb9b1caccdd970cccd6e2faad4694986bc8a7f3b
Ricky Drohan <rdrohanirl@yahoo.ie>
2024-08-03 15:33:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🍰 Machine Learning App')
 
-st.write('This is an app that builds a machine learning model')
+st.info('This is an app that builds a machine learning model')
